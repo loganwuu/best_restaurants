2c7f219659dfbaabba9d2ca47c9f4aca4f0a0548
logan wu & william swanson <student@epicodus.com>
2015-08-19 10:52:27 -0700

    wrote cuisine.php

---
+++ src/Cuisine.php
@@ -0,0 +1,85 @@
+<?php
+
+    class Cuisine
+    {
+        private $name;
+        private $id;
+
+        function __construct($name, $id=null)
+        {
+            $this->name = $name;
+            $this->id = $id;
+        }
+
+        function getId()
+        {
+            return $this->id;
+        }
+
+        function setId($new_id)
+        {
+            $this->id = $new_id;
+        }
+
+        function setName($new_name)
+        {
+            $this->name = (string) $new_name;
+        }
+
+        function getName()
+        {
+            return $this->name;
+        }
+
+        function getRestaurants()
+        {
+            $restaurants = array();
+            $returned_restaurants = $GLOBALS['DB']->query("SELECT * FROM restaurants WHERE cuisine_id={$this->getId()};");
+            foreach($returned_restaurants as $restaurant) {
+                $name = $restaurant['name'];
+                $id = $restaurant['id'];
+                $cuisine_id = $restaurant['cuisine_id'];
+                $new_Restaurant = new Restaurant($name, $id, $cuisine_id);
+                array_push($restaurants, $new_Restaurant);
+            }
+            return $restaurants;
+        }
+        function save()
+        {
+            $GLOBALS['DB']->exec("INSERT INTO cuisine (name) VALUES ('{$this->getName()}');");
+            $result_id = $GLOBALS['DB']->lastInsertId();
+            $this->setId($results_id);
+        }
+
+        static function getAll()
+        {
+            $returned_cuisines = $GLOBALS['DB']->query("SELECT * FROM cuisines;");
+            $cuisines = array();
+            foreach($returned_cuisines as $cuisine) {
+                $name = $cuisine['name'];
+                $id = $cuisine['id'];
+                $new_cuisine = new Cuisine($name, $id);
+                array_push($cuisines, $new_cuisine);
+            }
+            return $cuisines;
+        }
+
+        static function deleteAll()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM cuisines;");
+        }
+
+        static function find($search_id)
+        {
+            $found_cuisine = NULL;
+            $cuisines = Cuisine::getAll();
+            foreach($cuisines as $cuisine) {
+                $cuisine_id = $cuisine->getId();
+                if ($cuisine_id == $search_id) {
+                    $found_cuisine = $cuisine;
+                }
+            }
+            return $found_cuisine;
+        }
+    }
+?>
---
+++ tests/RestaurantTest.php
@@ -0,0 +1,28 @@
+<?php
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
+    require_once "src/Restaurant.php";
+    require_once "src/Cuisine.php";
+
+    $server = 'mysql:host=localhost;dbname=restaurants_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    class RestaurantTest extends PHPUnit_Framework_TestCase
+    {
+        protected function tearDown()
+        {
+            Restaurant::deleteAll();
+            Category::deleteAll();
+        }
+
+        function test_getId()
+        {
+            
+        }
+    }
+?>
